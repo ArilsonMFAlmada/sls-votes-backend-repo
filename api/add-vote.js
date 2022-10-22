b0ed7e6c444e63329556d24e5ccb6fd7b7ed842d
arilson <arilson.almada@hotmail.com>
2022-10-22 13:48:40 -0300

    get-votes development

--- api/add-vote.js
+++ api/add-vote.js
@@ -3,7 +3,6 @@ AWS.config.update({ region: 'us-east-1' });
 
 const moment = require('moment');
 const uuidv4 = require('uuid/v4');
-
 const util =  require('./util.js')
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
