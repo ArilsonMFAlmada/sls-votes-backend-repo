3dd9389db957c36af9152c7d981c38012bcbf4f9
arilson <arilson.almada@hotmail.com>
2022-10-22 18:49:29 -0300

    get-vote ans delete-vote development

--- api/get-vote.js
+++ api/get-vote.js
@@ -1,6 +1,7 @@
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'us-east-1' });
 
+const _ = require('underscore')
 const util =  require('./util.js')
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -8,11 +9,34 @@ const tableName = process.env.VOTES_TABLE;
 
 exports.handler = async (event) => {
     try {
-        return {
-            statusCode: 200,
-            headers: util.getResponseHeaders(),
-            body: JSON.stringify('')
+        let vote_id = decodeURIComponent(event.pathParameters.note_id);
+
+        let params = {
+            TableName: tableName,
+            IndexName: "vote_id-index",
+            KeyConditionExpression: "vote_id = :vote_id",
+            ExpressionAttributeValues: {
+                ":vote_id": vote_id
+            },
+            Limit: 1
+            
+        };
+
+        let data =  await dynamodb.query(params).promise();
+
+        if(!_.isEmpty(data.Items)) {
+            return {
+                statusCode: 200,
+                headers: util.getResponseHeaders(),
+                body: JSON.stringify(data.Items[0])
+            };
+        } else {
+            return {
+                statusCode: 404,
+                headers: util.getResponseHeaders()
+            }
         }
+       
     } catch (error) {
         console.log("Error", err)
         return {
